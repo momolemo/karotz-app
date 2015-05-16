6cc25e0cb79f5021d5e6db852e0a153d805b24f0
momolemo <vincent.montagne@gmail.com>
2015-05-16 18:03:11 +0200

    renommage du script d'intall

--- install/install_progTV.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROGDIR="/usr/www/cgi-bin/apps"
-PROGFILE="progtv"
-PROGURL="https://github.com/momolemo/karotz-app-TV/blob/master/cgi-bin/apps"
-
-wget -q -O ${PROGDIR}/${PROGFILE} ${PROGURL}/PROGFILE >>/dev/null 2>>/dev/null
