1e331141007bc3358a96ff2adc3df8996e63f288
momolemo <vincent.montagne@gmail.com>
2015-05-16 18:41:07 +0200

    ajout changement de droit apres telechargement prog

--- install/karotz-app-TV_install.sh
+++ install/karotz-app-TV_install.sh
@@ -5,3 +5,5 @@ PROGNAME="progtv"
 PROGURL="https://github.com/momolemo/karotz-app-TV/blob/master/cgi-bin/apps"
 
 curl -s -k ${PROGURL}/${PROGNAME} > ${PROGDIR}/${PROGNAME} 2>>/dev/null
+
+chmod 777 ${PROGDIR}/${PROGNAME}
